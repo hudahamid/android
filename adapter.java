f2af1e98e887d5ed3301998a610a3d6a62529229
hudahamid <36269806+hudahamid@users.noreply.github.com>
2024-05-09 02:21:10 +0300

    Update adapter.java

--- adapter.java
+++ adapter.java
@@ -1,4 +1,5 @@
- ListView listView = findViewById(R.id.listView);
+ArrayList<String> arr=new ArrayList<>();
+ListView listView = findViewById(R.id.listView);
  ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
  android.R.layout.simple_list_item_1, arr);
  listView.setAdapter(adapter);
